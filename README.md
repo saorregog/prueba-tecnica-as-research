6691739408a9f749ab881fcd4a1df21228967a8a
saorregog <saorregog@gmail.com>
2024-09-24 14:32:15 -0500

    third task done and improvements: slideshows behaves as expected

--- README.md
+++ README.md
@@ -4,12 +4,30 @@ Elaborada por Sergio Orrego
 
 ## Prueba 1 (fallo en el consumo de APIs)
 
-No todos los endpoints retornan los atributos de "Latitud" y "Longitud" para los registros consultados. Uno de los endpoints retorna los atributos "CoordMestricas_AL_N" y "CoordMestricas_AL_W", y los registros de otro de los endpoints pueden tener valores nulos o strings vacíos para alguno de los atributos anteriormente mencionados. Para los casos en los cuales alguno de los atributos no exista para un registro en particular, el valor sea nulo o un string vacío; se notifica al usuario mediante el texto "Sin información".
+No todos los endpoints retornan los atributos de `Latitud` y `Longitud` para los registros consultados. Uno de los endpoints retorna los atributos `CoordMestricas_AL_N` y `CoordMestricas_AL_W`, y los registros de otro de los endpoints pueden tener valores nulos o strings vacíos para algunos de los atributos anteriormente mencionados.
+
+Para solucionar este fallo, se decide notificar al usuario mediante el texto "Sin información" en aquellos casos en los cuales alguno de los atributos no exista para un registro en particular, el valor del atributo sea nulo o un string vacío.
 
 ## Prueba 2 (detección de fallos funcionales)
 
-Los endpoints retornan registros con el atributo "fotos" o "Fotos". Para que se rendericen todos los registros, fue necesario validar cuál de los dos atributos contiene cada registro en cuestión.
+Los endpoints retornan registros con el atributo `fotos` o `Fotos`. Para que se rendericen todos los registros, fue necesario validar cuál de los dos atributos contiene cada registro en cuestión y proseguir el flujo normal del aplicativo con la información del atributo correspondiente.
 
-Se hace además una optimización en este punto. Si un registro contiene material fotográfico (atributo "fotos" o "Fotos"), se calcula el número de fotos dentro del atributo para que sea recorrido eficientemente por el bucle "for".
+## Prueba 3 (fallo estético prioritario)
 
-Por último, si un registro no contiene material fotográfico, no se inicializa el slideshow para el mismo.
+El elemento con clase `slideshow-container` estaba renderizando todo el material fotográfico del registro y luego ocultándolo a medida que se desplazaba a la siguiente imagen para mostrar. Por lo tanto, se hizo algunas modificaciones a la lógica de la función `initializeSlideshows` para que se oculten todas las imágenes excepto la primera que debe ser mostrada, y luego se muestren las siguientes imágenes una a una cada vez que pasen 3 segundos.
+
+Con los cambios realizados, los elementos con clase `image-item` presentan desde el comienzo un tamaño uniforme.
+
+## Prueba 5 (optimización de recursos computacionales)
+
+- Se agrega un favicon al aplicativo.
+
+- El archivo .ttf con la tipografía Candara es recuperado de internet y ubicado en la raíz del proyecto.
+
+- Se crean archivos .css y .js para contener el código correspondiente y reducir la longitud del archivo .html.
+
+- Si un registro contiene material fotográfico (atributo `fotos` o `Fotos`), se calcula el número de fotos dentro del atributo para que sea recorrido eficientemente por el bucle `for`.
+
+- Si un registro no contiene material fotográfico, no se inicializa el dinamismo en el elemento con clase `slideshow-container`.
+
+- El evento de Click asociado al elemento con clase `slideshow-container` no se visualiza correctamente debido a que cada elemento de imagen perteneciente al elemento con clase `mySlides`, tiene un evento de Click asociado que se ejecuta al mismo tiempo y abre el modal con la imagen en cuestión a tamaño completo. Se decide que el evento asociado al elemento con clase `slideshow-container` se ejecute con Click Derecho.
---
+++ script.js
@@ -0,0 +1,133 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const imageContainer = document.getElementById("image-container");
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const closeBtn = document.getElementById("close");
+
+  const sources = [
+    "https://natural-connection-0f2ac61546.strapiapp.com/api/formato1-caracterizacions",
+    "https://natural-connection-0f2ac61546.strapiapp.com/api/formato2-complementos",
+    "https://natural-connection-0f2ac61546.strapiapp.com/api/especies-formato3s",
+    "https://natural-connection-0f2ac61546.strapiapp.com/api/formato5-monitoreo-restauracions",
+    "https://natural-connection-0f2ac61546.strapiapp.com/api/formato-4-hijos-semanas",
+  ];
+
+  // Function to fetch and display images from each source
+  function fetchAndDisplayImages(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.data.forEach((item) => {
+          const imageItem = document.createElement("div");
+          imageItem.className = "image-item";
+
+          const itemLatitude =
+            item.attributes?.Latitud ?? item.attributes?.CoordMestricas_AL_N;
+          const itemLongitude =
+            item.attributes?.Longitud ?? item.attributes?.CoordMestricas_AL_W;
+
+          const info = `
+                        <p style="font-weight: bold; font-family: sans-serif; color: #FF7401; font-size: 18px;"><strong></strong> ${
+                          item.attributes.createdAt
+                        }</p>
+                        <p style="font-weight: regular; font-family: sans-serif; font-size: 15px;"><strong>Latitud:</strong> ${
+                          itemLatitude === undefined || itemLatitude === ""
+                            ? "Sin información"
+                            : itemLatitude
+                        }</p>
+                        <p style="font-weight: regular; font-family: sans-serif; font-size: 15px;"><strong>Longitud:</strong> ${
+                          itemLongitude === undefined || itemLongitude === ""
+                            ? "Sin información"
+                            : itemLongitude
+                        }</p>
+                    `;
+
+          const slideshowContainer = document.createElement("div");
+          slideshowContainer.className = "slideshow-container";
+
+          const itemImages = item.attributes?.fotos ?? item.attributes?.Fotos;
+
+          if (itemImages) {
+            const itemImagesLength = Object.keys(itemImages).length;
+
+            for (let i = 0; i < itemImagesLength; i++) {
+              const imageData = itemImages[i];
+              if (imageData) {
+                const slide = document.createElement("div");
+                slide.className = "mySlides";
+
+                const imgElement = document.createElement("img");
+                imgElement.src = `data:image/jpeg;base64,${imageData}`;
+                imgElement.addEventListener("click", () => {
+                  modal.style.display = "flex";
+                  modalImg.src = imgElement.src;
+                });
+                slide.appendChild(imgElement);
+
+                slideshowContainer.appendChild(slide);
+              }
+            }
+          }
+
+          imageItem.innerHTML = info;
+          imageItem.appendChild(slideshowContainer);
+          imageContainer.appendChild(imageItem);
+        });
+
+        initializeSlideshows();
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
+
+  // Fetch images from all sources
+  sources.forEach((url) => fetchAndDisplayImages(url));
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+function initializeSlideshows() {
+  const slideshows = document.querySelectorAll(".slideshow-container");
+
+  slideshows.forEach((slideshow) => {
+    const slides = slideshow.querySelectorAll(".mySlides");
+
+    if (!slides.length) return;
+
+    slides[0].style.display = "block";
+
+    for (let i = 1; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    let currentSlide = 0;
+
+    function showSlide() {
+      slides[currentSlide].style.display = "none";
+      currentSlide = currentSlide + 1 === slides.length ? 0 : currentSlide + 1;
+      slides[currentSlide].style.display = "block";
+    }
+
+    let intervalId = setInterval(() => {
+      showSlide();
+    }, 3000);
+
+    slideshow.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      clearInterval(intervalId);
+
+      intervalId = setInterval(() => {
+        showSlide();
+      }, 3000);
+
+      showSlide();
+    });
+  });
+}
